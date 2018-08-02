7ddd7911c7f603d454ccc6473acbaea538da8d64
Dmitry Ivanov <dmitry@appnext.com>
2018-08-02 16:55:51 +0300

    add script for making VM using  "reddit-full" images

---
+++ config-scripts/create_reddit_vm.sh
@@ -0,0 +1,7 @@
+gcloud compute instances create reddit-app \
+--boot-disk-size=10GB \
+--image-family reddit-full \
+--image-project=infra-211620 \
+--machine-type=g1-small \
+--tags puma-server \
+--restart-on-failure
